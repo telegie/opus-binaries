02b04ed8add73e09e633659c71b30b4f7e8b9ba7
Hanseul Jun <hanseuljun@gmail.com>
2022-08-29 19:47:19 +0000

    Make fPIC really work.

--- scripts/configure-linux.sh
+++ scripts/configure-linux.sh
@@ -2,10 +2,9 @@
 
 ../opus/autogen.sh
 
-EXTRA_CFLAGS="-fPIC"
-
 ../opus/configure \
   --disable-shared \
   --disable-doc \
   --disable-extra-programs \
   --prefix="$(dirname $(pwd))/e4d4b74/x64-linux" \
+  CFLAGS="-fPIC"
